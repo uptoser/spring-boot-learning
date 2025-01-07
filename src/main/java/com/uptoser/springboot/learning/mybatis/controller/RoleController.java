3e0d4204b64743f5b3a7f76932822ca4531481d9
uptoser <450052550@qq.com>
2025-01-07 20:54:52 +0800

    mybatis

--- src/main/java/com/uptoser/springboot/learning/mybatis/controller/RoleController.java
+++ src/main/java/com/uptoser/springboot/learning/mybatis/controller/RoleController.java
@@ -1,9 +1,13 @@
 package com.uptoser.springboot.learning.mybatis.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.uptoser.springboot.learning.base.entity.ResponseEntity;
 import com.uptoser.springboot.learning.mybatis.entities.Role;
 import com.uptoser.springboot.learning.mybatis.service.IRoleService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,8 +26,21 @@ public class RoleController {
     IRoleService roleService;
 
     @RequestMapping(value = "/products", method = RequestMethod.POST)
+    @ApiOperation(value = "获取商品列表")
+    @ApiImplicitParams({ @ApiImplicitParam(paramType = "form",  name = "id", value = "商品id", required = true) })
     public ResponseEntity<List<Role>> insertingProducts() {
         return new ResponseEntity<>(200, "成功", roleService.list());
+    }
+
+    @RequestMapping(value = "/roles", method = RequestMethod.POST)
+    @ApiOperation(value = "获取角色列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "form",  name = "id", value = "商品id", required = true)
+    })
+    public ResponseEntity<List<Role>> roles() {
+        LambdaQueryWrapper<Role> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.like(Role::getRoleName, "role_name_1");
+        return new ResponseEntity<>(200, "成功", roleService.list(lambdaQueryWrapper));
     }
 
 }
--- src/main/java/com/uptoser/springboot/learning/mybatis/entities/Role.java
+++ src/main/java/com/uptoser/springboot/learning/mybatis/entities/Role.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -28,7 +29,7 @@ public class Role implements Serializable {
 
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
-
+    @ApiModelProperty(value = "商品名称" ,name = "roleName",example = "苹果电脑")
     private String roleName;
 
     private String note;
