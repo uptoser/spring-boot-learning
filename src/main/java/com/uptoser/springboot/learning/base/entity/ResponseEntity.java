cc633601ed2359457b508e5c808c4faab6f8299f
uptoser <450052550@qq.com>
2025-01-07 13:39:39 +0800

    增加一些swagger配置的注释

--- src/main/java/com/uptoser/springboot/learning/base/entity/ResponseEntity.java
+++ src/main/java/com/uptoser/springboot/learning/base/entity/ResponseEntity.java
@@ -1,51 +1,51 @@
-//package com.uptoser.springboot.learning.base.model;
-//
-//import io.swagger.annotations.ApiModelProperty;
-//
-//public class ResponseEntity<T> {
-//    @ApiModelProperty(value = "状态码")
-//    private int code;
-//    @ApiModelProperty(value = "消息")
-//    private String message;
-//    @ApiModelProperty(value = "结果")
-//    private T result;
-//
-//    public ResponseEntity(int code, String message, T result) {
-//        this.code = code;
-//        this.message = message;
-//        this.result = result;
-//    }
-//
-//    public int getCode() {
-//        return code;
-//    }
-//
-//    public void setCode(int code) {
-//        this.code = code;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-//
-//    public T getResult() {
-//        return result;
-//    }
-//
-//    public void setResult(T result) {
-//        this.result = result;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "ResponseEntity{" +
-//                "code=" + code +
-//                ", message='" + message + '\'' +
-//                ", result=" + result +
-//                '}';
-//    }
-//}
+package com.uptoser.springboot.learning.base.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class ResponseEntity<T> {
+    @ApiModelProperty(value = "状态码")
+    private int code;
+    @ApiModelProperty(value = "消息")
+    private String message;
+    @ApiModelProperty(value = "结果")
+    private T result;
+
+    public ResponseEntity(int code, String message, T result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseEntity{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", result=" + result +
+                '}';
+    }
+}
